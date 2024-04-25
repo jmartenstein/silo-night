1d001394738f4c4b2b63d31321fb3685c6641b0c
Justin Martenstein <jmartenstein@knockcrm.com>
2024-04-24 20:58:11 -0700

    add code and tests for shows

--- lib/show.rb
+++ lib/show.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Show
 
   attr_accessor :name
@@ -9,7 +11,47 @@ class Show
   end
 
   def average_runtime
-    return 25
+
+    # split the strings by space and hyphen
+    times = @runtime.split(/[\s-]/)
+
+    # remove minutes
+    times.delete("minutes")
+
+    # sum the remaining values:
+    sum = times.map(&:to_i).reduce(:+)
+
+    return sum / times.count()
+
+  end
+
+end
+
+class Shows
+  include Enumerable
+
+  attr_accessor :list
+
+  def initialize()
+    @list = []
+  end
+
+  def each( &block )
+    @list.each &block
+  end
+
+  def [](index)
+    @list[index] || "None"
+  end
+
+  def load_from_file( filename="" )
+    text = File.read(filename)
+    load_from_json(text)
+  end
+
+  def load_from_json( json="[]" )
+    j = JSON.parse(json)
+    j.each { |show| @list.append(Show.new(show)) }
   end
 
 end
--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -3,12 +3,12 @@ require 'rspec'
 
 describe Show do
 
-  let(:suits) {{ "name" => "Suits", "runtime" => "42 minutes" }}
+  let(:suits)    {{ "name" => "Suits",    "runtime" => "42 minutes"    }}
   let(:platonic) {{ "name" => "Platonic", "runtime" => "28-32 minutes" }}
 
   it "creates a new show with an empty name" do
-    n = Show.new.name
-    expect(n).to eq("")
+    n = Show.new
+    expect(n.name).to eq("")
   end
 
   it "loads from a data structure" do
@@ -24,6 +24,30 @@ describe Show do
   it "calculates an average from two numbers" do
     s = Show.new(platonic)
     expect(s.average_runtime).to eq(30)
+  end
+
+end
+
+describe Shows do
+
+  let(:shows)    { '[ { "name": "foo", "runtime": "12 minutes" } ]' }
+  let(:filename) { "spec/support/shows.json" }
+
+  it "initializes an empty list" do
+    l = Shows.new()
+    expect(l.count).to eq(0)
+  end
+
+  it "list loads from a file" do
+    l = Shows.new()
+    l.load_from_file(filename)
+    expect(l[0].name).to eq("foo")
+  end
+
+  it "list loads from json" do
+    l = Shows.new()
+    l.load_from_json(shows)
+    expect(l[0].runtime).to eq("12 minutes")
   end
 
 end
