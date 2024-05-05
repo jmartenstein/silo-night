af80db2f7847f7d92ec57e516b3448cccd9b87b1
Justin Martenstein <jmartenstein@knockcrm.com>
2024-05-05 08:28:34 -0700

    update seed data to match cucumber test

--- data/seed.sql
+++ data/seed.sql
@@ -1,17 +1,19 @@
 insert or ignore into users(
   id, name)
 values
-  (1, "Stephanie"),
-  (2, "Justin");
+  (1, "steph"),
+  (2, "justin");
 
 insert or ignore into shows(
   id, name, avg_duration)
 values
   (1, "Equalizer", 44),
-  (2, "Grey's Anatomy", 44);
+  (2, "Grey's Anatomy", 44),
+  (3, "Slow Horses", 42);
 
-insert or ignore into showlist(
+insert or ignore into usershows(
   id, user_id, show_id)
 values
   (1, 1, 1),
-  (2, 1, 2);
+  (2, 1, 2),
+  (3, 2, 3);
