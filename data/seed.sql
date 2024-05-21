c1a436d069cad094b4cee2fab15532e46d45cf0b
Justin Martenstein <jmartenstein@knockcrm.com>
2024-05-20 18:56:39 -0700

    add suits to steph's data

--- data/seed.sql
+++ data/seed.sql
@@ -5,7 +5,11 @@ insert into shows( id, name, runtime )
   values
     (NULL, "Equalizer", "43-44 minutes"),
     (NULL, "Grey's Anatomy", "43-53 minutes"),
-    (NULL, "Slow Horses", "41-53 minutes");
+    (NULL, "Slow Horses", "41-53 minutes"),
+    (NULL, "Suits", "44 minutes");
 
 insert into usershows( id, user_id, show_id )
-  values (NULL, 1, 1), (NULL, 1, 2), (NULL, 2, 3);
+  values (NULL, 1, 1),
+         (NULL, 1, 2),
+         (NULL, 2, 3),
+         (NULL, 1, 4);
