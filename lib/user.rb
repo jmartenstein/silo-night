dac4101a9ff51b70aaa627eced11ada5f635cb29
Justin Martenstein <jmartenstein@knockcrm.com>
2024-05-25 20:54:53 -0700

    add tests and code for user to generate a schedule

--- lib/user.rb
+++ lib/user.rb
@@ -8,9 +8,21 @@ class User
   attr_accessor :schedule
 
   def initialize( u={"config"=>{},"shows"=>[],"schedule"=>{}} )
-    @config   = u["config"]
-    @schedule = u["schedule"]
-    @shows    = u["shows"]
+    @config   = u["config"] || {}
+    @schedule = u["schedule"] || {}
+    @shows    = u["shows"] | []
+  end
+
+  def config_day_lookup( day="" )
+    case day
+      when "s";  "Sunday"
+      when "m";  "Monday"
+      when "t";  "Tuesday"
+      when "w";  "Wednesday"
+      when "th"; "Thursday"
+      when "f";  "Friday"
+      when "sa"; "Saturday"
+    end
   end
 
   def add_show( show="" )
@@ -60,20 +72,74 @@ class User
     # create a lookup from the test data
     lookup = Shows.new()
     lookup.load_from_file("spec/support/shows.json")
-    temp = []
+
+    temp = Shows.new()
 
     # build a temporary list of shows from the lookup
     @shows.each do |show| 
-      temp.append( lookup.find { |k,v| v = show } )
+      lookup_show = lookup.find { |i| i.name == show }
+      temp.list.append lookup_show if not lookup_show.nil?
     end
     @shows = temp
 
-    return true
+    true
+
+  end
+
+  def is_show_in_schedule?( show="" )
+    @schedule.keys.each { |day| true if @schedule[day].include?( show ) }
+    false
+  end
+
+  def get_available_runtime_for_day( day="" )
+
+    # check to see if we need to add runtime information to the shows
+    expand_shows() if (@shows.count == 0) or (@shows[0].class == String)
+
+    day_schedule = @schedule[day]
+    runtime_sum = 0
+
+    if not day_schedule.nil?
+      day_schedule.each do |show|
+        lookup_show = @shows.find { |i| i.name == show }
+        runtime_sum += lookup_show.average_runtime
+      end
+    end
+
+    @config["time"].to_i - runtime_sum
+
+  end
+
+  def find_next_available_slot( show="" )
+
+    # check to see if we need to add runtime information to the shows
+    expand_shows() if (@shows.count == 0) or (@shows[0].class == String)
+
+    lookup_show = @shows.find { |i| i.name == show }
+    next_available_day = ""
+
+    @config["days"].each do |day|
+      avail = get_available_runtime_for_day(config_day_lookup(day))
+      if lookup_show.average_runtime <= avail
+        next_available_day = config_day_lookup(day)
+        break
+      end
+    end
+
+    next_available_day
 
   end
 
   def generate_schedule()
-    @schedule = { "monday" => ["foo"], "tuesday" => ["bar"] }
+    @shows.each do |show|
+
+      if not is_show_in_schedule?(show)
+        day = find_next_available_slot(show)
+        @schedule[day] = [] unless @schedule.key?(day)
+        @schedule[day].push(show)
+      end
+
+    end
   end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,10 +3,16 @@ require 'rspec'
 
 describe User do
 
-  let(:testc1) {{ "config"  => { "days" => "t", "time" => "10m" },
-                  "shows"   => [ "foo" ] }}
-  let(:testc2) {{ "config"  => { "days" => "t", "time" => "12m" },
-                  "shows"   => [ "bar" ] }}
+  let(:testc1) {{ "config"   => { "days" => [ "t", "w" ], "time" => "10m" },
+                  "shows"    => [ "foo", "bar", "baz" ],
+                  "schedule" => {
+                     "Tuesday" => [ "foo" ],
+                     "Wednesday" => [ "baz" ]
+                   }
+               }}
+  
+  let(:testc2) {{ "config"  => { "days" => [ "t", "w" ], "time" => "12m" },
+                  "shows"   => [ "bar", "foo" ] }}
 
   it "creates a new user with an empty config" do
     u = User.new
@@ -23,10 +29,20 @@ describe User do
     expect(u.shows).to eq(testc1["shows"])
   end
 
+  it "loads a schedule from a file" do
+
+    u = User.new()
+    u.load_from_file("./data/justin.json")
+    sched = u.schedule
+
+    expect(sched["monday"]).to include("Reacher")
+
+  end
+
   it "deletes a show from the list" do
-    u = User.new(testc1)
-    u.delete_show("foo")
-    expect(u.shows).to be_empty
+    u = User.new(testc2)
+    u.delete_show("bar")
+    expect(u.shows).to include("foo")
   end
 
   it "add a show to the list" do
@@ -40,19 +56,60 @@ describe User do
     u.expand_shows()
     expect(u.shows[0].class.name).to eq("Show")
     expect(u.shows[0].runtime).to eq ("4-6 minutes")
+    expect(u.shows[1].class.name).to eq("Show")
+    expect(u.shows[1].runtime).to eq ("5 minutes")
+  end
+
+  it "finds next available slot for user 1" do
+    u = User.new(testc1)
+    expect(u.find_next_available_slot("bar")).to eq("Tuesday")
+  end
+
+  it "finds next available slot fo user 2" do
+    u = User.new(testc2)
+    expect(u.find_next_available_slot("bar")).to eq("Tuesday")
+  end
+
+  it "checks if time is available for Wednesday" do  
+    u = User.new(testc1)
+    expect(u.get_available_runtime_for_day("Wednesday")).to eq(7)
+  end
+
+  it "checks if Tuesday is full in second user's schedule" do
+
+    u = User.new(testc2)
+    u.generate_schedule
+
+    actual = u.get_available_runtime_for_day("Tuesday")
+    expect(actual).to eq(2)
+
   end
 
   it "generates a first populated schedule" do
+
     u = User.new(testc1)
+    weekday = "Tuesday"
+
     u.generate_schedule()
+
     expect(u.schedule).not_to be_nil
-    expect(u.schedule["tuesday"][0]).to eq("foo")
+    expect(u.schedule[weekday]).not_to be_empty
+    expect(u.schedule[weekday].length).to eq(2)
+    expect(u.schedule[weekday]).to include("foo")
+
   end
 
   it "generates a second populated schedule" do
+
     u = User.new(testc2)
+    weekday = "Tuesday"
+
     u.generate_schedule()
-    expect(u.schedule["tuesday"][0]).to eq("bar")
+    #p u.schedule
+    
+    expect(u.schedule[weekday]).not_to be_empty
+    expect(u.schedule[weekday][0]).to eq("bar")
+
   end
 
 end
