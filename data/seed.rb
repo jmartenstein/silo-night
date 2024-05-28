1a9cfdb2043fed5657c0ade3f19e298fbb737863
Justin Martenstein <jmartenstein@knockcrm.com>
2024-05-27 20:19:20 -0700

    update seed script to be idempotent

--- data/seed.rb
+++ data/seed.rb
@@ -13,13 +13,19 @@ def load_user(name)
 
   u = User.new
   u.load_from_file_by_username(name)
-  u.save unless User.find(name: name)
 
-  j = JSON.parse(File.read("data/#{name}.json"))
-  u = User.find(name: name)
-  j["shows"].each { |s| u.add_show( Show.find(name: s)) }
+  if not User.find(name: name) then
+
+    u.save
+
+    j = JSON.parse(File.read("data/#{name}.json"))
+    u = User.find(name: name)
+    j["shows"].each { |s| u.add_show( Show.find(name: s)) }
+
+  end
 
 end
 
 load_user("justin")
 load_user("test")
+load_user("steph")
