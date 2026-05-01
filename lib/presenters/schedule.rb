10d79b453b938b6e2b5a3f80bc4421e5172cc5e2
Justin Martenstein <jmartenstein@gmail.com>
2026-05-01 16:33:59 -0700

    Complete API v1 Milestone 2: Integrated Presenters::Show, added unit tests, and updated architecture docs

--- lib/presenters/schedule.rb
+++ lib/presenters/schedule.rb
@@ -1,3 +1,6 @@
+require 'presenters/show'
+require 'show'
+
 module Presenters
   class Schedule
     DAYS = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday].freeze
@@ -8,7 +11,21 @@ module Presenters
 
     def to_h
       DAYS.each_with_object({}) do |day, memo|
-        memo[day] = @schedule[day] || @schedule[day.to_sym] || []
+        raw_shows = @schedule[day] || @schedule[day.to_sym] || []
+        
+        memo[day] = raw_shows.map do |show_data|
+          # Look up the real show object by name
+          show_name = show_data.is_a?(Hash) ? show_data['name'] : show_data
+          show = ::Show.find(name: show_name)
+          
+          if show
+            # Use the Show presenter for consistent formatting
+            Presenters::Show.new(show).to_h
+          else
+            # Fallback for data we can't find
+            show_data
+          end
+        end
       end
     end
 
