7f49753b69c1c407681e131d80d25f6fc42887b4
Justin Martenstein <jmartenstein@gmail.com>
2026-05-08 17:36:42 -0400

    clean up search services spec

--- spec/lib/presenters/search_result_spec.rb
+++ spec/lib/presenters/search_result_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 require 'presenters/search_result'
 
 RSpec.describe Presenters::SearchResult do
-  let(:search_results) { [{ 'title' => 'The Expanse' }, { 'title' => 'Foundation' }] }
+  let(:search_results) { [{ name: 'The Expanse' }, { name: 'Foundation' }] }
   let(:presenter) { described_class.new(search_results) }
 
   
-  it 'formats search results into a standardized JSON array', failing: true do
+  it 'formats search results into a standardized JSON array' do
     result = presenter.to_h
     expect(result).to be_an(Array)
-    expect(result.first).to eq({ 'name' => 'The Expanse' })
+    expect(result.first).to eq({
+      'name' => 'The Expanse',
+      'year' => nil,
+      'genres' => nil,
+      'poster_path' => nil
+    })
   end
 end
